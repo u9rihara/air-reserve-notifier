4cf33bfcc509f94e422ef7758f2b81dedad2249c
Yu Kurihara <kurihara@uzu-lab.co.jp>
2019-02-12 11:21:11 +0900

    LINE通知実装

--- dist/Code.js
+++ dist/Code.js
@@ -1,3 +1,17 @@
-function myFunction() {
+function main() {
 
+}
+
+function getStore(key) {
+  var properties = PropertiesService.getScriptProperties();
+  return properties.getProperty(key);
+}
+
+function setStore(key, value) {
+  var properties = PropertiesService.getScriptProperties();
+  return properties.setProperty(key, value);
+}
+
+function test() {
+  notifyLine('test');
 }
--- dist/Line.js
+++ dist/Line.js
@@ -1,3 +1,15 @@
-function myFunction() {
-
+/**
+ * LINE通知
+ * tokenはスクリプトプロパティ LINE_NOTIFY_TOKEN に格納する
+ * @param {string} message メッセージ本文
+ */
+function notifyLine(message) {
+  var token = getStore('LINE_NOTIFY_TOKEN');
+  UrlFetchApp.fetch('https://notify-api.line.me/api/notify', {
+    method: 'post',
+    headers: {
+      Authorization: 'Bearer ' + token
+    },
+    payload: 'message=' + message
+  });
 }
